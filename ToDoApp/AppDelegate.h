46eee281b0c9197fb94553ca35e763f930679316
Justin Leach <justin.leach@intouchsol.com>
2015-05-19 20:11:27 -0500

    Initial Commit

---
+++ ToDoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoApp
+//
+//  Created by Justin Leach on 5/19/15.
+//  Copyright (c) 2015 Justin Leach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoApp
+//
+//  Created by Justin Leach on 5/19/15.
+//  Copyright (c) 2015 Justin Leach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
