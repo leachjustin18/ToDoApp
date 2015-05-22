e03bc01c4f0a4b72091d4e91c45d6c4af496338a
Justin Leach <justin.leach@intouchsol.com>
2015-05-21 20:13:07 -0500

    Updated numberOfRowsInSection to return how many items we have in toDoItems array.

--- ToDoApp/ToDoListTableViewController.m
+++ ToDoApp/ToDoListTableViewController.m
@@ -65,9 +65,8 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return [self.toDoItems count];
 }
 
 /*
