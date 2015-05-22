b1179293a74edf10ca7eeec35dc291d4c202d10d
Justin Leach <justin.leach@intouchsol.com>
2015-05-21 19:48:56 -0500

    Added ToDoItem cocoa touch class with nsobject as the parent class

---
+++ ToDoApp/ToDoItem.h
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.h
+//  ToDoApp
+//
+//  Created by Justin Leach on 5/21/15.
+//  Copyright (c) 2015 Justin Leach. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ToDoItem : NSObject
+
+@end
---
+++ ToDoApp/ToDoItem.m
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.m
+//  ToDoApp
+//
+//  Created by Justin Leach on 5/21/15.
+//  Copyright (c) 2015 Justin Leach. All rights reserved.
+//
+
+#import "ToDoItem.h"
+
+@implementation ToDoItem
+
+@end
